0e2c825ee821ec208f68b0c5af77bc0c31b21ac1
animusarya <souravarya4u@gmail.com>
2019-08-22 01:07:49 -0700

    minor fixes

--- src/components/cta.js
+++ src/components/cta.js
@@ -3,9 +3,9 @@ import React from "react"
 import SectionHeader from "./section-header"
 
 const CallToAction = () => (
-  <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
+  <div style={{ padding: "0rem 1rem", textAlign: "center" }}>
     <SectionHeader
-      title="Why Packers ?"
+      title="Why Packers?"
       description="We are a product delivery platform trying to take all your transportation problems away."
     />
   </div>
